04b2cbfff8b1c135beab9b768a5790e72ac93221
hewigovens <hewigovens@gmail.com>
2017-09-23 20:20:02 +0800

    Initial Commit

---
+++ AnyTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnyTime
+//
+//  Created by Tao Xu on 9/23/17.
+//  Copyright © 2017 Tao Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
